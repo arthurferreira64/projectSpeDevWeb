15aeeb84bcc5ef1eebccc248911b3b1b70afb08b
Arthur Ferreira <arthur@my-infinity.eu>
2024-04-26 14:43:51 +0200

    vu product

--- front/product/one-product/one-product.js
+++ front/product/one-product/one-product.js
@@ -17,12 +17,40 @@ document.addEventListener('DOMContentLoaded', async () => {
         const product = await getProductById(productId);
         if (product) {
             document.querySelector("#details").innerHTML = `
-                    <h1 style="text-align: center; font-size: xx-large;"> Information de ${product.name} </h1>
-                   <div style="margin-left: 10px">
-                    <p>${product.description}</p>
-                    <p>${product.price} €</p>
+                <div class="container mx-auto mt-10 shadow-lg" style="padding-bottom: 1rem; width: 50%">
+                     <h1 class="text-2xl font-bold mb-4" style="text-align: center">
+                        Information de ${product.name}
+                    </h1>
+                <div class="max-w-lg mx-auto my-10">
+                    <div class="mb-4">
+                        <label for="name" class="block font-bold mb-2">
+                            Nom : ${product.name}
+                        </label>
+                    </div>
+                    <div class="mb-4">
+                        <label for="name" class="block font-bold mb-2">
+                            Catégorie : ${product.categorie}
+                        </label>
+                    </div>
+                    <div class="mb-4">
+                        <label for="name" class="block font-bold mb-2">
+                            Description : ${product.description}
+                        </label>
+                    </div>
+                    <div class="mb-4">
+                     <label for="name" class="block font-bold mb-2"
+                     >Prix : ${product.price} €</label>
+                    </div>
+                  
                 </div>
-            `;
+`;
+            const imageGallery = document.getElementById('image');
+            product.images.forEach(imageUrl => {
+                const imageElement = document.createElement('img');
+                imageElement.src = imageUrl;
+                imageElement.alt = product.name;
+                imageGallery.appendChild(imageElement);
+            });
         } else {
             productDetailsElement.innerHTML = '<p>Le produit n`existe pas!</p>';
         }
